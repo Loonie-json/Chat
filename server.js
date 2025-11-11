63aae9ce855e82f4a0a976eae49dee7ea7d2849a
Alysonmbs <alyson.matheus06@gmaul.com>
2025-11-11 07:04:11 -0300

    bah

--- server.js
+++ server.js
@@ -97,8 +97,8 @@ wss.on("connection", ws => {
         
         if (data.tipo === "ia") {
           async function chamarIA(prompt) {
-            let CO = `Responda em português: ${prompt}`
-          let api = await fetch(`https://shizuku-apis.shop/api/ias/gpt?texto=${prompt}&apitoken=Loon-dev`)
+            let CO = `Responda em pt-br: ${prompt}`
+          let api = await fetch(`https://shizuku-apis.shop/api/ias/gpt?texto=${CO}&apitoken=Loon-dev`)
           let resultado = await api.json()
           let resposta = await resultado.resultado.data[0].resposta
           return resposta
